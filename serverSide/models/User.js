9b9157dec58c65ad809b726846669c99e35c8cd8
Miguel Sabino <miguelsabino2008@gmail.com>
2025-07-24 02:18:37 -0300

    esqueci de relacionar as entidades kk

--- serverSide/models/User.js
+++ serverSide/models/User.js
@@ -7,7 +7,11 @@ const User = mongoose.model('User', {
     level: {type: Number, default: 1},
     rank: {type: String, enum:['Non-Rank', 'Bronze3', 'Bronze2', 'Bronze1', 'Silver3', 'Silver2', 'Silver1', 'Gold3', 'Gold2', 'Gold1', 'Platinum3', 'Platinum2', 'Platinum1', 'Diamond3', 'Diamond2', 'Diamond1', 'Savant3', 'Savant2', 'Savant1', "The Elder"], default:'Non-Rank'},
     xp: {type: Number, default: 0},
-    img: {type: String, default: "https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1"}
+    img: {type: String, default: "https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1"},
+    folders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Folder'
+    }]
 })
 
 module.exports = User
